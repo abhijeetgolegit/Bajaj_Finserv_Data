8ca7758883f904ebc481686c626875d759181dae
abhijeetgolegit <117170792+abhijeetgolegit@users.noreply.github.com>
2023-02-08 16:09:04 +0530

    Delete 007FunctionParameters.js

--- Day I/007FunctionParameters.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function Add(x, y) {
-    if (x === void 0) { x = 0; }
-    if (y === void 0) { y = 0; }
-    return x + y;
-}
-console.log(Add(3, 4));
-console.log(Add(3));
-console.log(Add());
