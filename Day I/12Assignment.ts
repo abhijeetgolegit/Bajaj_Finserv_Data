a94cf6ce3db633ba11ea07c1ba146916a6aaaa76
abhijeetgolegit <117170792+abhijeetgolegit@users.noreply.github.com>
2023-02-08 17:57:05 +0530

    Add files via upload

---
+++ Day I/12Assignment.ts
@@ -0,0 +1,14 @@
+//Create a function Reverse which can accept string or any type
+// of array and give the following kind of o/p
+
+function Reverse(strOrArr:any){
+
+}
+
+console.log(Reverse("Abhijeet"));                 
+// [ 't', 'e', 'e', 'j', 'i', 'h','b','A' ]
+console.log(Reverse(["PQR", "XYZ", "ABC"]));    
+// [ 'ABC', 'XYZ', 'PQR' ]
+console.log(Reverse([10, 20, 30, 40]));        
+ // [ 40, 30, 20, 10 ]
+console.log(Reverse(10)); //Compile time Error
